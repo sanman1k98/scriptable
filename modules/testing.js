48b2dee2793a5fb732488edc2de61054801977c0
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-07-02 21:37:22 -0400

    Add modules directory
    Test Scriptable's support for ES6 modules. Only Scriptable's `module`
    object and `importModule` function are supported in scripts. Even
    trying to use the dynamic `import()` function does not work. It
    results in an error saying that there is no module loader.

---
+++ modules/testing.js
@@ -0,0 +1,6 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: gray; icon-glyph: magic;
+module.exports.test = () => {
+    console.log("Hello from the 'testing' module!")
+} 
---
+++ test_runner.js
@@ -0,0 +1,5 @@
+// Variables used by Scriptable.
+// These must be at the very top of the file. Do not edit.
+// icon-color: brown; icon-glyph: magic;
+const testing = importModule("modules/testing")
+testing.test()
