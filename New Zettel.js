1689ed492633af91a7106d788c92790ae4351bd1
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-07-03 00:44:48 -0400

    Refactor
    Remove unused functions, intermediate variables, and seemingly
    uneccessary comments.

--- New Zettel.js
+++ New Zettel.js
@@ -5,21 +5,15 @@
 
 const writer = importModule("modules/writer")
 
-// Path to directory to create new zettels.
 const dir = "/Locations/iCloud/zk/"
 const ext = ".md"
 
-const now = new Date()
-const id = createID(now)
-console.log(id)
-
-const zettelPath = dir + id + ext
+const filename = createID(new Date())
+const zettelPath = dir + filename + ext
 
 const url = writer.editNewFileURL(zettelPath)
-console.log(url)
 
 Safari.open(url)
-
 Script.complete()
 
 
@@ -27,16 +21,4 @@ function createID(date) {
   const df = new DateFormatter()
   df.dateFormat = "yyyyMMddHHmmss"
   return df.string(date)
-}
-
-// Pads `num` with zeros to target length of 2.
-function pad(num) {
-  return num.toString().padStart(2, 0)
-}
-
-function openNewFile(path) {
-  let url = "ia-writer://new?path="
-  url += path
-  url = encodeURI(url)
-  Safari.open(url)
 }
