180086896567ccae672f54d3e9d8d176ca6cb7fd
Sandeep Dilip <34922961+sanman1k98@users.noreply.github.com>
2022-07-03 01:21:28 -0400

    More refactoring

--- modules/writer.js
+++ modules/writer.js
@@ -1,19 +1,21 @@
+const scheme = "iawriter://"
+
+function param (name, value) {
+    return name + "=" + value
+}
+
+// Returns an URL that, when opened, creates a new file at the given path containing the optionally given text.
 module.exports.editNewFileURL = (path, text = "") => {
-    const scheme = "iawriter://"
 
     const command = "new"
     
-    let queryParams = []
-    
-    queryParams.push(param("path", path))
-    queryParams.push(param("text", text))
-    queryParams.push(param("edit", "true"))
+    let queryParams = [
+        param("path", path),
+        param("text", text),
+        param("edit", "true")
+    ] 
     
     const url = scheme + command + "?" + queryParams.join("&")
     
     return encodeURI(url)
-}
-
-function param (name, value) {
-    return name + "=" + value
 }
